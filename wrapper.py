7eedc620997afe0cf8b08a282636e9653229d4fb
Andrew Haberlandt <andrew.haberlandt@gmail.com>
2023-07-10 09:56:59 +0200

    Wrapper script uses proper temporary files for reduced formula and proofs

--- wrapper.py
+++ wrapper.py
@@ -1,7 +1,7 @@
 
 import argparse
 import subprocess
-
+from tempfile import NamedTemporaryFile
 
 def find_stats(fname):
     with open(fname, 'r') as f:
@@ -23,66 +23,65 @@ def run_solver_bare(args):
 
 
 def run_solver_reduced(args, reduced_cnf, reduced_drat):
-    p = subprocess.run([
-        args.solver,
-        reduced_cnf,
-        'solver.drat',
-        '--no-binary'
-    ], stdout=subprocess.PIPE)
+    with NamedTemporaryFile() as solver_drat:
+        p = subprocess.run([
+            args.solver,
+            reduced_cnf,
+            solver_drat.name,
+            '--no-binary'
+        ], stdout=subprocess.PIPE)
 
-    rcode = p.returncode
+        rcode = p.returncode
 
-    if rcode not in [0,10,20]:
-        # If solver failed, run on original input
-        run_solver_bare(args)
-    else:
-        res = p.stdout.decode('latin-1')
-
-        if rcode == 20:
-            # UNSAT: Fixup the DRAT proof
-            proof = ''
-            with open(reduced_drat, 'r') as f:
-                proof += f.read()
-            with open('solver.drat', 'r') as f:
-                proof += f.read()
-            
-            with open(args.output, 'w') as f:
-                f.write(proof)
-            
-            print('s UNSATISFIABLE')
+        if rcode not in [0,10,20]:
+            # If solver failed, run on original input
+            run_solver_bare(args)
         else:
-            # SAT: Remove auxiliary variables from solution
-            nv, _ = find_stats(args.input)
+            res = p.stdout.decode('latin-1')
 
-            lits = []
-            for line in res.split('\n'):
-                if line.startswith('v'):
-                    line_lits = line.split(' ')[1:]
-                    lits += [int(l) for l in line_lits if abs(int(l)) <= nv]
-            out = "s SATISFIABLE\nv " + " ".join(map(str, lits)) + "\n"
+            if rcode == 20:
+                # UNSAT: Fixup the DRAT proof
+                proof = ''
+                with open(reduced_drat, 'r') as f:
+                    proof += f.read()
+                with open(solver_drat.name, 'r') as f:
+                    proof += f.read()
 
-            print(out)
+                with open(args.output, 'w') as f:
+                    f.write(proof)
+
+                print('s UNSATISFIABLE')
+            else:
+                # SAT: Remove auxiliary variables from solution
+                nv, _ = find_stats(args.input)
+
+                lits = []
+                for line in res.split('\n'):
+                    if line.startswith('v'):
+                        line_lits = line.split(' ')[1:]
+                        lits += [int(l) for l in line_lits if abs(int(l)) <= nv]
+                out = "s SATISFIABLE\nv " + " ".join(map(str, lits)) + "\n"
+
+                print(out)
 
 
 def run(args):
-    # Run BVA
-    p = subprocess.run([
-        'timeout', str(args.t2), args.bva,
-        '-i', args.input,
-        '-o', 'bva.cnf',
-        '-p', 'bva.drat',
-        '-t', str(args.t1),
-    ])
+    with NamedTemporaryFile() as bva_cnf, NamedTemporaryFile() as bva_drat:
+        # Run BVA
+        p = subprocess.run([
+            'timeout', str(args.t2), args.bva,
+            '-i', args.input,
+            '-o', bva_cnf.name,
+            '-p', bva_drat.name,
+            '-t', str(args.t1),
+        ])
 
-    if p.returncode == 0:
-        # BVA ran successfully
-        run_solver_reduced(args, 'bva.cnf', 'bva.drat')
-    else:
-        # Run original solver on input
-        run_solver_bare(args)
-
-    # Cleanup
-    subprocess.run(['rm', '-f', 'bva.cnf', 'bva.drat', 'solver.drat'])
+        if p.returncode == 0:
+            # BVA ran successfully
+            run_solver_reduced(args, bva_cnf.name, bva_drat.name)
+        else:
+            # Run original solver on input
+            run_solver_bare(args)
 
 
 if __name__=='__main__':
